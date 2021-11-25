25242a94a99b4852d50ef4c82c234b1a96143e8e
raullapeira <raul.lapeira@gmail.com>
2021-11-25 10:31:47 +0100

    Cambios para API basica
    Ya tenemos listar y alta

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/listar")
+def listar():
+    f = open("demofile.txt", "r")
+    return f.read()
+
+@app.post("/alta/{nombre}/{telefono}")
+def dar_alta(nombre: str, telefono: str):
+    f = open("demofile.txt", "w")
+    # {
+    # "nombre": "pepe",
+    # "telefono": "5551234"
+    # }
+    f.write("{\"nombre\":\"" + nombre + "\",\"telefono\":\"" + telefono + "\"}")
+    f.close()
+    return {"alta":"ok"}
